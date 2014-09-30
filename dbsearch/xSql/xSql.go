1fb9eee43438210eb81e1b4d0c7b9c2b43c4d739
Vlad Lashko <ostrovok@MacBook-Pro-Vladimir.local>
2014-09-30 16:51:17 +0400

    Add new modul for create SQL request

---
+++ dbsearch/xSql/xSql.go
@@ -0,0 +1,146 @@
+package xSql
+
+import (
+	"fmt"
+	"github.com/iostrovok/go-iutils/iutils"
+	"log"
+	"strings"
+)
+
+var MarkList = map[string]bool{
+	"IS":    true,
+	"IN":    true,
+	"LIKE":  true,
+	"ILIKE": true,
+	"=":     true,
+	">=":    true,
+	"<=":    true,
+	"<>":    true,
+	">":     true,
+	"<":     true,
+}
+
+var LogicList = map[string]bool{
+	"AND": true,
+	"OR":  true,
+}
+
+type One struct {
+	Data  []interface{}
+	Field string
+	Type  string
+}
+
+func IN(field string, data []interface{}) *One {
+	one := One{}
+	one.Data = data
+	one.Type = "IN"
+	one.Field = field
+
+	return &one
+}
+
+func (one *One) Comp(PointIn ...int) (string, []interface{}) {
+
+	Point := 1
+	if len(PointIn) > 0 {
+		Point = PointIn[0]
+	}
+
+	sqlLine := ""
+	values := []interface{}{}
+	switch one.Type {
+	case "AND", "OR":
+		s := []string{}
+		for _, v := range one.Data {
+			switch v.(type) {
+			case *One:
+				sql, vals := v.(*One).Comp(Point)
+				Point += len(vals)
+				s = append(s, sql)
+				if len(vals) > 0 {
+					values = append(values, vals...)
+				}
+			default:
+				log.Fatalf("Comp. Not defined %T\n", v)
+				log.Fatalf("Comp. Not defined %v\n", v)
+			}
+		}
+		sqlLine = "( " + strings.Join(s, " "+one.Type+" ") + ") "
+	case "IN":
+		s := []string{}
+		i := len(one.Data)
+		for {
+			s = append(s, fmt.Sprintf(" $%d ", Point))
+			Point++
+			i--
+			if i < 1 {
+				break
+			}
+		}
+		sqlLine = fmt.Sprintf(" %s IN ( %s ) ", one.Field, strings.Join(s, ", "))
+		values = one.Data
+	case "IS":
+		sqlLine = one.Field + " IS " + iutils.AnyToString(one.Data[0])
+	default:
+		sqlLine = fmt.Sprintf(" %s %s $%d ", one.Field, one.Type, Point)
+		Point++
+		values = append(values, one.Data[0])
+	}
+
+	return sqlLine, values
+}
+
+func (one *One) Append(Nexters ...*One) *One {
+	if _, find := LogicList[one.Type]; !find {
+		log.Fatalf("Append. Bad type for append %s\n", one.Type)
+	}
+	for _, v := range Nexters {
+		one.Data = append(one.Data, v)
+	}
+	return one
+}
+
+func NLogic(mark string, Nexters ...*One) *One {
+	one := One{}
+	one.Logic(mark, Nexters...)
+	return &one
+}
+
+func (one *One) Logic(mark string, Nexters ...*One) {
+
+	if _, find := LogicList[mark]; !find {
+		log.Fatalf("Logic. Not defined %s\n", mark)
+	}
+
+	one.Data = []interface{}{}
+	for _, v := range Nexters {
+		one.Data = append(one.Data, v)
+	}
+	one.Type = mark
+}
+
+func Mark(field string, mark string, data ...interface{}) *One {
+	In := One{}
+
+	if _, find := MarkList[mark]; find {
+		In.Type = mark
+	} else {
+		log.Fatalf("Mark. Not defined %s\n", mark)
+	}
+
+	In.Data = data
+	In.Field = field
+
+	if mark == "IS" {
+		if iutils.AnyToString(In.Data[0]) == "NULL" {
+			In.Data = []interface{}{"NULL"}
+		} else if iutils.AnyToString(In.Data[0]) == "NOT NULL" {
+			In.Data = []interface{}{"NOT NULL"}
+		} else {
+			log.Fatalf("Mark. Not defined %s. You have to use 'IS', 'NULL' or 'IS', 'NOT NULL' \n", mark)
+		}
+	}
+
+	return &In
+}
---
+++ dbsearch/xSql/xSql_test.go
@@ -0,0 +1,33 @@
+package xSql
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func TestWhere(t *testing.T) {
+
+	var test_line = "((((fias_id=$1orfias_id>=$2orfias_id<$3orfias_id<=$4ornamein($5)orfias_idilike$6orfias_idlike$7)andid<$8)orfirst_canmeisnotnull)andsecond_anmeisnull)"
+	c := Mark("fias_id", "=", "test_eq")
+	c1 := Mark("fias_id", ">=", "test_eg")
+	c2 := Mark("fias_id", "<", "test_lt")
+	c3 := Mark("fias_id", "<=", "test_el")
+	c4 := Mark("fias_id", "ILIKE", "test_el")
+	c5 := Mark("fias_id", "LIKE", "test_el")
+	t1 := NLogic("OR", c, c1, c2, c3).Append(Mark("name", "IN", "John"), c4, c5)
+	t2 := NLogic("AND").Append(t1).Append(Mark("id", "<", 100))
+	t3 := NLogic("OR").Append(t2).Append(Mark("first_canme", "IS", "NOT NULL"))
+	top := NLogic("AND").Append(t3).Append(Mark("second_anme", "IS", "NULL"))
+	sql, values := top.Comp()
+
+	var N = regexp.MustCompile(`\s+`)
+	st := strings.ToLower(N.ReplaceAllString(sql, ""))
+
+	if test_line != st {
+		t.Fatal("error xSql: sqlLine")
+	}
+	if len(values) != 8 {
+		t.Fatal("error xSql: values")
+	}
+}
