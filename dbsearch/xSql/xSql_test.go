9619852a0d072eabb35df1837f4642c12392e5b2
Vlad Lashko <ostrovok@MacBook-Pro-Vladimir.local>
2014-10-09 11:01:51 +0400

    Rewrites tests

--- dbsearch/xSql/xSql_test.go
+++ dbsearch/xSql/xSql_test.go
@@ -8,50 +8,74 @@ import (
 	"testing"
 )
 
-func TestWhere(t *testing.T) {
-
-	var test_line = "((((fias_id=$1orfias_id>=$2orfias_id<$3orfias_id<=$4ornamein($5)orfias_idilike$6orfias_idlike$7)andid<$8)orfirst_canmeisnotnull)andsecond_anmeisnullandstartdate<now()andenddate>now())"
-	c := Mark("fias_id", "=", "test_eq")
-	c1 := Mark("fias_id", ">=", "test_eg")
-	c2 := Mark("fias_id", "<", "test_lt")
-	c3 := Mark("fias_id", "<=", "test_el")
-	c4 := Mark("fias_id", "ILIKE", "test_el")
-	c5 := Mark("fias_id", "LIKE", "test_el")
-	t1 := NLogic("OR", c, c1, c2, c3).Append(Mark("name", "IN", "John"), c4, c5)
-	t2 := NLogic("AND").Append(t1).Append(Mark("id", "<", 100))
-	t3 := NLogic("OR").Append(t2).Append(Mark("first_canme", "IS", "NOT NULL"))
-	top := NLogic("AND").Append(t3).Append(Mark("second_anme", "IS", "NULL"))
-	sql, values := top.Append(Func("startdate < now()")).Append(Func("enddate > now()")).Comp()
+func check_result(t *testing.T, sql1 string, sql2 string, values []interface{}, count int) {
+	log.Printf("%s\n", sql1)
+	log.Printf("%s\n", sql2)
+	log.Printf("%v\n", values)
 
 	var N = regexp.MustCompile(`\s+`)
-	st := strings.ToLower(N.ReplaceAllString(sql, ""))
+	s1 := strings.TrimSpace(strings.ToLower(N.ReplaceAllString(sql1, "")))
+	s2 := strings.TrimSpace(strings.ToLower(N.ReplaceAllString(sql2, "")))
 
-	if test_line != st {
-		t.Fatal("error where xSql: sqlLine")
+	if s1 != s2 {
+		t.Fatal("error where xSql: sqlLine for " + sql2)
 	}
-	if len(values) != 8 {
-		t.Fatal("error where xSql: values")
+	if len(values) != count {
+		t.Fatal("error where xSql: values for " + sql2)
+	}
+}
+
+func TestWhere(t *testing.T) {
+
+	sql := ""
+	values := []interface{}{}
+
+	/* check select */
+	marks := []string{
+		"LIKE", "ILIKE", "=", ">=", "<=", "<>", ">", "<",
 	}
 
-	var insert_line = "insertintomytable(first_f,enddate,second_f,nextdate)values($1,now(),$2,interval'1day'+now())returning*,nextdateased"
-	in := Insert("mytable")
-	in.Append(Mark("first_f", "=", 10)).Append(Mark("enddate", "SQL", "now()")).Append(Mark("second_f", "=", "SUPER"))
-	in.Append(Mark("nextdate", "SQL", "interval '1 day' + now()"))
-	in.Append(Mark("*", "RET", ""))
-	in.Append(Mark("nextdate as ED", "RET", ""))
-
-	sql, values = in.Comp()
-
-	log.Println(sql)
-	//spew.Dump(values)
-
-	st = strings.ToLower(N.ReplaceAllString(sql, ""))
-	log.Println(st)
-	log.Println(insert_line)
-	if insert_line != st {
-		t.Fatal("error insert xSql: sqlLine")
+	for _, m := range marks {
+		sql, values = Mark("f", m, "1").Comp()
+		check_result(t, sql, " f "+m+" $1 ", values, 1)
 	}
-	if len(values) != 2 {
-		t.Fatal("error insert xSql: values")
+
+	/* check IN */
+	sql, values = Mark("f", "IN", 1, 2, "3").Comp()
+	check_result(t, sql, " f IN ( $1, $2, $3 ) ", values, 3)
+
+	/* check IS */
+	sql, values = Mark("f", "IS", "NULL").Comp()
+	check_result(t, sql, " f IS NULL", values, 0)
+
+	sql, values = Mark("f", "IS", "NOT NULL").Comp()
+	check_result(t, sql, " f IS NOT NULL", values, 0)
+
+	/* check logic condition */
+	sql, values = NLogic("OR").Append(Mark("f", "=", "1")).Append(Mark("t", "<=", "2")).Comp()
+	check_result(t, sql, "( f = $1  OR  t <= $2 )", values, 2)
+
+	sql, values = NLogic("AND").Append(Mark("f", "<>", "1")).Append(Mark("t", ">=", "2")).Comp()
+	check_result(t, sql, "( f <> $1 AND t >= $2 )", values, 2)
+
+	/* check condition with function */
+	sql, values = NLogic("AND").Append(Func("startdate < now()")).Append(Func("enddate > now()")).Comp()
+	check_result(t, sql, "( startdate < now() AND enddate > now() )", values, 0)
+
+	/* check array */
+	array_marks := []string{
+		"=", "<>", "<", ">", "<=", ">=", "@>", "<@", "&&", "||",
 	}
+	for _, m := range array_marks {
+		sql, values = Array("f", m, 1, 2, 3).Comp()
+		check_result(t, sql, "f "+m+" ARRAY[ $1, $2, $3 ]", values, 3)
+	}
+
+	/* test insert */
+	insert := Insert("public.mytable").Append(Mark("a", "=", 1)).Append(Array("b", "=", 1, 2, 3)).Append(Array("c", "="))
+	insert.Append(Mark("*", "RET", "")).Append(Mark("b as d", "RET", ""))
+	sql, values = insert.Comp()
+	check_result(t, sql, "INSERT INTO public.mytable ( a, b, c ) VALUES ($1, ARRAY[$2, $3, $4], '{}') RETURNING *, b as d", values, 4)
+
+	//t.Fatal("error insert xSql: text view")
 }
