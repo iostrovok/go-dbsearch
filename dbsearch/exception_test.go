e7bbd57bab02a37bc4a175ba484bce10ee63bb53
Vlad Lashko <ostrovok@gmail.com>
2015-01-27 18:58:06 +0300

    Adds test for check exception

--- dbsearch/exception_test.go
+++ dbsearch/exception_test.go
@@ -1,6 +1,9 @@
 package dbsearch
 
 import (
+	"log"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
@@ -9,6 +12,7 @@ func Test_Exception(t *testing.T) {
 	s.SetDebug(false)
 	if s != nil {
 		//_01_exception_load_test(t, s)
+		//_02_exception_load_test(t, s)
 	}
 	//t.Fatal("Success [no error] test")
 }
@@ -17,6 +21,7 @@ func Test_Exception(t *testing.T) {
 	Check SType field
 */
 func _01_exception_load_test(t *testing.T, s *Searcher) {
+
 	var autoload_mTestType *AllRows = &AllRows{}
 
 	var err error = nil
@@ -30,4 +35,40 @@ func _01_exception_load_test(t *testing.T, s *Searcher) {
 		t.Fatalf("No catch error for empty SType\n")
 	}
 
+}
+
+type exception_05_TestPlace struct {
+	Col1 int `db:"col1" type:"serial"`
+	Col2 int `db:"col2" type:"int"`
+}
+
+var exception_05_mTestType *AllRows = &AllRows{
+	SType: reflect.TypeOf(exception_05_TestPlace{}),
+}
+
+func _02_exception_load_test(t *testing.T, s *Searcher) {
+	runtime.GOMAXPROCS(1)
+
+	sql_create := " CREATE TABLE public.test " +
+		"( col1 serial, col2 text )"
+
+	sql_cols := "INSERT INTO public.test(col2) "
+	sql_vals := []string{
+		" VALUES ('ee')",
+		" VALUES ('ee')",
+		" VALUES ('ee')",
+	}
+
+	make_t_table(s, sql_create, sql_cols, sql_vals)
+
+	p := []exception_05_TestPlace{}
+	s.GetFork(exception_05_mTestType, &p, "SELECT * FROM public.test ORDER BY 1")
+
+	face, err := s.GetFaceFork(exception_05_mTestType, "SELECT * FROM public.test ORDER BY 1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Printf("\n%#v\n", face)
+
+	//check_face_json(t, face[0], "_21_exception_test")
 }
